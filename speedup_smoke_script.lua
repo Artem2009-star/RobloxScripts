980b42f7cc8ae95f7bf1c4f24958e61708cffa79
Artem2009-star <artemzir35@gmail.com>
2025-03-03 19:31:56 +0200

    Update speedup_smoke_script.lua

--- speedup_smoke_script.lua
+++ speedup_smoke_script.lua
@@ -1,9 +1,26 @@
+local toolName = "SuperSpeedCoil"
 local player = game.Players.LocalPlayer
 local backpack = player.Backpack
 
--- Перевірка всіх елементів у рюкзаку
+local toolFound = false
 for _, item in pairs(backpack:GetChildren()) do
-    if item:IsA("Tool") then
-        print(item.Name)
+    if item:IsA("Tool") and item.Name == toolName then
+        toolFound = true
+        print(toolName .. " found in backpack!")
+        break
     end
+end
+
+if toolFound then
+    local tool = backpack:FindFirstChild(toolName)
+    local part = tool.Handle
+    local smoke = Instance.new("Smoke")
+    smoke.Parent = part
+    smoke.Opacity = 0.5
+    smoke.RiseVelocity = 5
+    smoke.Size = 10
+    smoke.Color = Color3.fromRGB(150, 150, 150)
+    smoke.Enabled = true
+else
+    print(toolName .. " not found in backpack!")
 end
